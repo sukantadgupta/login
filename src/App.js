7fc8fafbbd757cc27c3931c832b25caee65d3cff
Sukanta01 <72244847+Sukanta01@users.noreply.github.com>
2023-05-02 07:26:45 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import { useState } from 'react';
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!email || !password || !confirmPassword) {
+      setError('Please fill in all fields');
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setError('Passwords do not match');
+      return;
+    }
+
+    // try {
+    //   await firebase.auth().createUserWithEmailAndPassword(email, password);
+    //   console.log('User has successfully signed up.');
+    // } catch (error) {
+    //   console.error(error);
+    //   setError(error.message);
+    // }
+
+    let url;
+    // if (isLogin) {
+    //   url =
+    //     'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDK8-gBCjUvMtsxYYRNZrnQavLMv3Slw6w';
+    // } else {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDK8-gBCjUvMtsxYYRNZrnQavLMv3Slw6w';
+    // }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = 'Authentication failed!';
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        const expirationTime = new Date(
+          new Date().getTime() + +data.expiresIn * 1000
+        );
+        console.log(data.idToken);
+        // authCtx.login(data.idToken, expirationTime.toISOString());
+        // history('/');
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+
+      <div>
+        <label htmlFor="confirm-password">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirm-password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+      </div>
+
+      {error && <div style={{ color: 'red' }}>{error}</div>}
+
+      <button type="submit">Sign Up</button>
+    </form>
   );
 }
+
 
 export default App;
